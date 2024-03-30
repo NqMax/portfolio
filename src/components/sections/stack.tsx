ed457a3a89c8821141353c8bd78217f0cef46bc4
Max <111196993+NqMax@users.noreply.github.com>
2024-03-30 15:05:25 -0600

    Refactored stack section

--- src/components/sections/stack.tsx
+++ src/components/sections/stack.tsx
@@ -1,5 +1,4 @@
 import { Section } from "@/components/section";
-import { Technology } from "@/components/technology";
 // Icons
 import { FaCss3Alt, FaHtml5, FaReact, FaNodeJs } from "react-icons/fa";
 import { PiFileSql } from "react-icons/pi";
@@ -14,50 +13,50 @@ import {
 const stackList = [
   {
     name: "TypeScript",
-    icon: <SiTypescript className="text-blue-500 h-full w-full" />,
+    icon: <SiTypescript className="h-full w-full text-blue-500" />,
   },
   {
     name: "JavaScript",
-    icon: <SiJavascript className="text-yellow-500 h-full w-full" />,
+    icon: <SiJavascript className="h-full w-full text-yellow-500" />,
   },
   {
     name: "CSS",
-    icon: <FaCss3Alt className="text-blue-800 h-full w-full" />,
+    icon: <FaCss3Alt className="h-full w-full text-blue-800" />,
   },
   {
     name: "HTML",
-    icon: <FaHtml5 className="text-red-500 h-full w-full" />,
+    icon: <FaHtml5 className="h-full w-full text-red-500" />,
   },
   {
     name: "Node",
-    icon: <FaNodeJs className="text-green-500 h-full w-full" />,
+    icon: <FaNodeJs className="h-full w-full text-green-500" />,
   },
   {
     name: "React",
-    icon: <FaReact className="text-blue-500 h-full w-full" />,
+    icon: <FaReact className="h-full w-full text-blue-500" />,
   },
   {
     name: "Next",
-    icon: <SiNextdotjs className="text-black h-full w-full" />,
+    icon: <SiNextdotjs className="h-full w-full text-black" />,
   },
   {
     name: "Tailwind",
-    icon: <SiTailwindcss className="text-sky-400 h-full w-full" />,
+    icon: <SiTailwindcss className="h-full w-full text-sky-400" />,
   },
   {
     name: "SQL",
-    icon: <PiFileSql className="text-red-500 h-full w-full" />,
+    icon: <PiFileSql className="h-full w-full text-red-500" />,
   },
   {
     name: "Express",
-    icon: <SiExpress className="text-yellow-500 h-full w-full" />,
+    icon: <SiExpress className="h-16 w-16 text-yellow-500" />,
   },
 ];
 
 export function Stack() {
   return (
     <Section number="03" title="My Stack" id="stack">
-      <ul className="grid-cols-5 grid justify-items-center gap-y-6 p-4 bg-white/10 shadow backdrop-blur rounded">
+      <ul className="grid grid-cols-5 justify-items-center gap-y-6 rounded bg-white/10 p-3 shadow backdrop-blur">
         {stackList.map((tech) => (
           <li key={tech.name}>
             <Technology name={tech.name} icon={tech.icon} />
@@ -65,5 +64,14 @@ export function Stack() {
         ))}
       </ul>
     </Section>
+  );
+}
+
+function Technology({ name, icon }: { name: string; icon: React.ReactNode }) {
+  return (
+    <div className="flex h-16 w-16 flex-col items-center gap-y-2">
+      {icon}
+      <div className="text-sm">{name}</div>
+    </div>
   );
 }
