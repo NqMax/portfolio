d3b847730ef603882fabffd9ecea076431f2725c
Max <111196993+NqMax@users.noreply.github.com>
2024-03-30 15:05:54 -0600

    Modified rel attribute to include noopner

--- src/components/sections/socials.tsx
+++ src/components/sections/socials.tsx
@@ -13,7 +13,7 @@ export function Socials() {
     <ul className="flex gap-x-4" aria-label="Social media">
       {socials.map((social) => (
         <li key={social.name}>
-          <a href={social.url} target="_blank" rel="noreferrer">
+          <a href={social.url} target="_blank" rel="noreferrer noopener">
             <span className="sr-only">{social.name}</span>
             <social.icon className="hover:text-accent h-6 w-6 transition-colors" />
           </a>
