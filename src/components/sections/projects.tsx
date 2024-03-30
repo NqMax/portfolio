eaf10141d3746ce12f46607d9a7b350e739da2fa
Max <111196993+NqMax@users.noreply.github.com>
2024-03-30 15:06:28 -0600

    Refactored projects section to render from a list of projects

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,75 +1,59 @@
 import { Section } from "@/components/section";
 import { Project } from "@/components/project";
 // Icons
-import { FaCss3Alt, FaHtml5, FaDocker, FaPhp, FaNodeJs } from "react-icons/fa";
+import {
+  FaCss3Alt,
+  FaHtml5,
+  FaReact,
+  FaDocker,
+  FaPhp,
+  FaNodeJs,
+} from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
-import { SiTailwindcss, SiExpress } from "react-icons/si";
+import { SiTailwindcss, SiNextdotjs, SiTypescript } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 
 export function Projects() {
+  const projects = [
+    {
+      title: "Poké Search",
+      picture: "/poke-search.png",
+      url: "https://poke-search-theta.vercel.app/",
+      technologies: [
+        { name: "React", icon: <FaReact className="text-sky-500" /> },
+        {
+          name: "Tailwind CSS",
+          icon: <SiTailwindcss className="text-sky-400" />,
+        },
+        { name: "Next.js", icon: <SiNextdotjs className="text-black" /> },
+        {
+          name: "TypeScript",
+          icon: <SiTypescript className="text-blue-500" />,
+        },
+        { name: "HTML", icon: <FaHtml5 className="text-red-500" /> },
+        { name: "CSS", icon: <FaCss3Alt className="text-blue-500" /> },
+      ],
+      description:
+        "Web application that allows users to search for Pokémon through a beautiful interface. Created using the popular PokéAPI restful interface.",
+    },
+  ];
+
   return (
     <Section number="04" title="My Projects" id="projects">
-      <div className="flex flex-col gap-y-14 mt-3">
-        <Project
-          title="Coming Soon!"
-          technologies={[
-            {
-              name: "JavaScript",
-              icon: <IoLogoJavascript className="text-yellow-500" />,
-            },
-            { name: "CSS", icon: <FaCss3Alt className="text-blue-800" /> },
-            { name: "HTML", icon: <FaHtml5 className="text-red-500" /> },
-          ]}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cumque
-          iure, maiores repudiandae voluptate alias reprehenderit cupiditate.
-        </Project>
-        <Project
-          title="Coming Soon!"
-          technologies={[
-            {
-              name: "Docker",
-              icon: <FaDocker className="text-blue-500" />,
-            },
-            { name: "Node", icon: <FaNodeJs className="text-green-500" /> },
-            {
-              name: "Express",
-              icon: <SiExpress className="text-yellow-500" />,
-            },
-          ]}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cumque
-          iure, maiores repudiandae voluptate alias reprehenderit cupiditate.
-        </Project>
-        <Project
-          title="Coming Soon!"
-          technologies={[
-            {
-              name: "Tailwind",
-              icon: <SiTailwindcss className="text-sky-400" />,
-            },
-            { name: "MySQL", icon: <GrMysql className="text-blue-500" /> },
-            { name: "PHP", icon: <FaPhp className="text-blue-500" /> },
-          ]}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cumque
-          iure, maiores repudiandae voluptate alias reprehenderit cupiditate.
-        </Project>
-        <Project
-          title="Coming Soon!"
-          technologies={[
-            {
-              name: "JavaScript",
-              icon: <IoLogoJavascript className="text-yellow-500" />,
-            },
-            { name: "CSS", icon: <FaCss3Alt className="text-blue-800" /> },
-            { name: "HTML", icon: <FaHtml5 className="text-red-500" /> },
-          ]}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cumque
-          iure, maiores repudiandae voluptate alias reprehenderit cupiditate.
-        </Project>
-      </div>
+      <ul className="mt-3 flex flex-col gap-y-12">
+        {projects.map((project) => (
+          <li key={project.title}>
+            <Project
+              title={project.title}
+              picture={project.picture}
+              url={project.url}
+              technologies={project.technologies}
+            >
+              {project.description}
+            </Project>
+          </li>
+        ))}
+      </ul>
     </Section>
   );
 }
