c8d40d770077ba14824bcdd57f80cdd88b2bd20b
Max <111196993+NqMax@users.noreply.github.com>
2024-04-09 13:20:57 -0600

    Style changes

--- src/components/sections/intro.tsx
+++ src/components/sections/intro.tsx
@@ -1,7 +1,7 @@
 export function Intro() {
   return (
     <div className="flex flex-col gap-y-4">
-      <div className="text-secondary">Hi, my name is</div>
+      <div className="text-secondary font-medium">Hi, my name is</div>
       <div className="text-accent flex flex-col gap-y-3 tracking-tight">
         <h1 className="text-5xl font-bold">Max Marroquín</h1>
         <h2 className="text-xl font-medium">Aspiring Full Stack Engineer</h2>
