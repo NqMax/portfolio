ce9d6c989c1c9e20077ae12390fcae9b5e9f0a4b
Max <111196993+NqMax@users.noreply.github.com>
2024-04-09 13:20:40 -0600

    Open footer refrence on a new tab

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,14 @@ export function Footer() {
     <footer>
       <p className="text-sm">
         Made with <span className="text-slate-300">Tailwind</span> and{" "}
-        <span className="text-slate-300">React</span> using <span className="text-slate-300">Next.js</span>. Inspired by the designs
+        <span className="text-slate-300">React</span> using{" "}
+        <span className="text-slate-300">Next.js</span>. Inspired by the designs
         of{" "}
         <a
           href="https://github.com/bchiang7"
-          className="text-slate-300 hover:text-secondary transition-all"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="text-slate-300 transition hover:text-secondary"
         >
           Brittany Chiang
         </a>
