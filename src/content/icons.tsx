f80e6dd704da9eef28e9e9f266f8f0e41270946f
Max <111196993+NqMax@users.noreply.github.com>
2024-04-10 02:22:02 -0600

    Created custom technology icons file

---
+++ src/content/icons.tsx
@@ -0,0 +1,12 @@
+// Icons
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs, SiTypescript } from "react-icons/si";
+
+export const HTMLIcon = <FaHtml5 className="text-red-500" />;
+export const JavaScriptIcon = <IoLogoJavascript className="text-yellow-400" />;
+export const CSSIcon = <FaCss3Alt className="text-blue-600" />;
+export const TailwindIcon = <SiTailwindcss className="text-sky-400" />;
+export const ReactIcon = <FaReact className="text-sky-500" />;
+export const NextIcon = <SiNextdotjs className="text-black" />;
+export const TypeScriptIcon = <SiTypescript className="text-blue-500" />;
