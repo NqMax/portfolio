ec2d8229fe609cf260be51d5ba468c7670497e7c
Max <111196993+NqMax@users.noreply.github.com>
2024-04-10 03:48:02 -0600

    Fixed icon colors not appearing

--- src/content/icons.tsx
+++ src/content/icons.tsx
@@ -4,9 +4,9 @@ import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs, SiTypescript } from "react-icons/si";
 
 export const HTMLIcon = <FaHtml5 className="text-red-500" />;
-export const JavaScriptIcon = <IoLogoJavascript className="text-yellow-400" />;
-export const CSSIcon = <FaCss3Alt className="text-blue-600" />;
+export const JavaScriptIcon = <IoLogoJavascript color="#facc15" className="text-yellow-400" />
+export const CSSIcon = <FaCss3Alt color="#2563eb" className="text-blue-600" />;
 export const TailwindIcon = <SiTailwindcss className="text-sky-400" />;
-export const ReactIcon = <FaReact className="text-sky-500" />;
+export const ReactIcon = <FaReact color="#0ea5e9" className="text-sky-500" />;
 export const NextIcon = <SiNextdotjs className="text-black" />;
 export const TypeScriptIcon = <SiTypescript className="text-blue-500" />;
