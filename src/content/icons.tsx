5c9637b703edd900972e6d502ddcdd7d49cc0d17
Max <111196993+NqMax@users.noreply.github.com>
2024-04-12 01:05:22 -0600

    Added icons

--- src/content/icons.tsx
+++ src/content/icons.tsx
@@ -1,7 +1,8 @@
 // Icons
 import { IoLogoJavascript } from "react-icons/io5";
-import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs, SiTypescript } from "react-icons/si";
+import { FaHtml5, FaCss3Alt, FaReact, FaNodeJs} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs, SiTypescript, SiExpo, SiExpress } from "react-icons/si";
+import { TbFileTypeSql } from "react-icons/tb";
 
 export const HTMLIcon = <FaHtml5 className="text-red-500" />;
 export const JavaScriptIcon = <IoLogoJavascript color="#facc15" className="text-yellow-400" />
@@ -10,3 +11,7 @@ export const TailwindIcon = <SiTailwindcss className="text-sky-400" />;
 export const ReactIcon = <FaReact color="#0ea5e9" className="text-sky-500" />;
 export const NextIcon = <SiNextdotjs className="text-black" />;
 export const TypeScriptIcon = <SiTypescript className="text-blue-500" />;
+export const ExpoIcon = <SiExpo className="text-black" />;
+export const NodeIcon = <FaNodeJs color="#8cc84b" className="text-green-500" />;
+export const ExpressIcon = <SiExpress className="text-black" />;
+export const SQLIcon = <TbFileTypeSql color="#f97316" className="text-orange-500" />;
