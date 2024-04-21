b0c03dee70398632ec2a6410c6e7a192acd7d242
Max <111196993+NqMax@users.noreply.github.com>
2024-04-21 03:09:09 -0600

    Added blur design to section titles on smaller screens

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -10,11 +10,13 @@ export function Section({
   id: string;
 }) {
   return (
-    <section className="flex scroll-mt-24 flex-col gap-y-4" id={id}>
-      <div className="flex items-center font-bold">
-        <span className="mr-2 text-secondary font-mono">{number}.</span>
-        <h3 className="text-accent mr-4">{title}</h3>
-        <hr className="grow" />
+    <section className="flex scroll-mt-24 flex-col lg:gap-y-4" id={id}>
+      <div className="sticky top-0 -mx-8 flex items-center py-4 font-bold backdrop-blur lg:static lg:-mx-0 lg:py-0 lg:backdrop-blur-none">
+        <span className="ml-8 mr-2 font-mono text-secondary lg:ml-0">
+          {number}.
+        </span>
+        <h3 className="mr-4 text-accent">{title}</h3>
+        <hr className="mr-8 grow lg:mr-0" />
       </div>
       {children}
     </section>
